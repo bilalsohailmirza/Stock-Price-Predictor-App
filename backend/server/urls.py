a2677fcd0631d074a6c22c17482488984507d826
Owais Ali Khan <owaisalikhan2003@gmail.com>
2024-05-01 21:39:01 +0500

    Added views for getting stock data in dataframe.

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.myView),
+    path('myView/', views.myView),
+    path('', views.graph),
 ]
--- backend/server/views.py
+++ backend/server/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from .models import Stocks
+from .logics import generate_graphs
+import json
 
 # Create your views here.
 def myView(request):
-    return HttpResponse("Test view")
+    stocks = Stocks.objects.all()
+    data = [stock.volume for stock in stocks]
+    return HttpResponse(f"Test view. {str(data[:10])}")
+
+def graph(request):
+    companies = ["AAPL"]
+    df = generate_graphs(companies)
+    return JsonResponse(df.iloc[1:].head().to_json(), safe=False)
+    # return HttpResponse("None")
